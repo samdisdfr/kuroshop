e9f88c04f28b232b07aad56acc2c8fe57b735d85
samdisdfr <hachahuisandro@gmail.com>
2023-12-14 19:02:16 -0500

    cambios en el html

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Si estás usando módulos ES6
+
+
 const url="http://localhost:5000/api/usuarios"
 
 function registrar() {
@@ -40,3 +43,56 @@ function enviar(){
         console.log(error);
     });
 }
+
+//productos
+
+window.onload = async()=> {
+  const productList = await(await fetch("http://localhost:5000/api/productos")).json();
+  console.log(productList);
+  display(productList);
+}
+
+function display(productList) {
+  let productoHTML = '';
+
+
+  productList.forEach(element => {
+    productoHTML += `
+      <div class="col-md-4">
+        <div class="card mb-4 product-wap rounded-0">
+          <div class="card rounded-0">
+            <img class="card-img rounded-0 img-fluid" src="${element.imagenes}">
+            <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
+              <ul class="list-unstyled">
+                <li><a class="btn btn-success text-white" href="${element.linkHeart}"><i class="far fa-heart"></i></a></li>
+                <li><a class="btn btn-success text-white mt-2" href="${element.linkEye}"><i class="far fa-eye"></i></a></li>
+                <li><a class="btn btn-success text-white mt-2" href="${element.linkCart}"><i class="fas fa-cart-plus"></i></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="card-body">
+            <a href="${element.link}" class="h3 text-decoration-none">${element.nombre}</a>
+            <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
+              <li class="pt-2">
+              <span class="product-color-dot color-dot-red float-left rounded-circle ml-1"></span>
+              </li>
+            </ul>
+            <ul class="list-unstyled d-flex justify-content-center mb-1">
+              <li>
+                ${Array.from({ length: element.rating }, (_, index) => `<i class="text-warning fa fa-star"></i>`).join('')}
+                ${Array.from({ length: 5 - element.rating }, (_, index) => `<i class="text-muted fa fa-star"></i>`).join('')}
+              </li>
+            </ul>
+            <p class="text-center mb-0">${element.precio}</p>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  // Agregar el HTML generado al contenedor deseado
+  const container = document.getElementById('productContainer');
+  container.innerHTML = productoHTML;
+}
+
+
